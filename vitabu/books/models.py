ccbb27e5003fe1a730cada15efa90fba85513f03
Nyambura🌻 <bianca@NyambusMacBook.local>
2022-08-13 01:13:38 +0300

    update models and run migrations

--- vitabu/books/models.py
+++ vitabu/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=100)
+  synopsis = models.CharField(max_length=1000)
+
+class Review(models.Model):
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+  comment = models.CharField(max_length=1000)
+  votes = models.IntegerField(default=0)
+
+
