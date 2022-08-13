83c4a71a94ec570b8684514380a2304d9c551798
Nyambura🌻 <bianca@NyambusMacBook.local>
2022-08-13 13:21:07 +0300

    update models and run migrations

--- vitabu/books/models.py
+++ vitabu/books/models.py
@@ -7,9 +7,15 @@ class Book(models.Model):
   author = models.CharField(max_length=100)
   synopsis = models.CharField(max_length=1000)
 
+  def __str__(self):
+        return self.title
+
 class Review(models.Model):
   book = models.ForeignKey(Book, on_delete=models.CASCADE)
   comment = models.CharField(max_length=1000)
   votes = models.IntegerField(default=0)
+
+  def __str__(self):
+        return self.book
 
 
